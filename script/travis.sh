ba3aef6f44c7436f82f531fe5435e8fe6487f843
Cameron Dutro <camertron@gmail.com>
2018-05-22 12:19:34 -0700

    Don't attempt to pull existing image

--- script/travis.sh
+++ script/travis.sh
@@ -8,7 +8,6 @@ echo $TRAVIS_COMMIT > REVISION
 export COMMIT=${TRAVIS_COMMIT::8}
 
 set -x
-docker pull quay.io/$docker_repo_name:$TRAVIS_BRANCH || docker pull quay.io/$docker_repo_name:master
 docker build -t quay.io/$docker_repo_name:$TRAVIS_BRANCH .
 docker tag quay.io/$docker_repo_name:$TRAVIS_BRANCH quay.io/$docker_repo_name:$COMMIT
 docker tag quay.io/$docker_repo_name:$COMMIT quay.io/$docker_repo_name:travis-$TRAVIS_BUILD_NUMBER
