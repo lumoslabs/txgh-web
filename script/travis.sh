5a549dad75ebd2c06ba44a89b2261b2d35adac4c
Cameron Dutro <camertron@gmail.com>
2018-05-22 12:15:23 -0700

    Add travis config

---
+++ script/travis.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -ev
+
+docker_repo_name='lumoslabs/txgh-web'
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD quay.io
+echo $TRAVIS_COMMIT > REVISION
+export COMMIT=${TRAVIS_COMMIT::8}
+
+set -x
+docker pull quay.io/$docker_repo_name:$TRAVIS_BRANCH || docker pull quay.io/$docker_repo_name:master
+docker build -t quay.io/$docker_repo_name:$TRAVIS_BRANCH .
+docker tag quay.io/$docker_repo_name:$TRAVIS_BRANCH quay.io/$docker_repo_name:$COMMIT
+docker tag quay.io/$docker_repo_name:$COMMIT quay.io/$docker_repo_name:travis-$TRAVIS_BUILD_NUMBER
+docker push quay.io/$docker_repo_name
